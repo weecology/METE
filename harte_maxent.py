2b265b8ff1aa1166c73eb50f17cdd59431cd85dd
Ethan White <ethan@weecology.org>
2010-06-09 21:39:42 +0000

    Initial commit of harte_maxent.py, a module for working with Harte et al.'s maximum entropy based macroecology models. Includes a single function, get_lambda_1, that solves for the primary parameter.

---
+++ harte_maxent.py
@@ -0,0 +1,24 @@
+# Module for fitting and testing Harte et al.'s maximum entropy models
+
+from __future__ import division
+from math import log
+from scipy.optimize import fsolve
+
+def get_lambda_1(S, N, approx='no'):
+    '''Solve for lambda_1 from Harte et al. 2008 using minimal approximations
+    based on their equation B.4'''
+    assert type(S) is int, "S must be an integer"
+    assert type(N) is int, "N must be an integer"
+    assert S > 1, "S must be greater than 1"
+    assert N > 0, "N must be greater than 0"
+    assert S/N < 1, "N must be greater than S"
+    assert approx == 'yes' or approx == 'no', """approx must be 'yes', 'no', or
+    blank"""
+    if approx == 'no':
+        y = lambda x: 1/log(1/(1-x))*x/(1-x)-N/S # x = e**-lambda_1 in B.4
+    else:
+        y = lambda x: x*log(1/x)-S/N
+    XSTART = 0.999999
+    exp_neg_lambda_1 = fsolve(y, XSTART)
+    lambda_1 = -1*log(exp_neg_lambda_1)
+    return lambda_1
