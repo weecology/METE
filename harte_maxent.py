1dddd509db1e155c52a679b27e01ec07d708af81
Ethan White <ethan@weecology.org>
2010-06-11 04:23:20 +0000

    changed numerical method in harte_maxent.get_lambda_1 to bracketing and bisection because fsolve was failing to converge for most starting points.
    completed first draft of harte_maxent.get_lambda_P, but currently broken because as entered the function does not have any roots.

--- harte_maxent.py
+++ harte_maxent.py
@@ -2,10 +2,16 @@
 
 # To Do:
 #    See if applying A.5 to B.1 yeilds an assumption free soln to lambda_1
+#    get_lambda_P does not find a root because it appears that there is no root
+#       for equation B.5 - investigate
 
 from __future__ import division
 from math import log
-from scipy.optimize import fsolve
+from scipy.optimize import bisect
+
+# Set the distance from the undefined boundaries of the Lagrangian multipliers
+# to set the upper and lower boundaries for the numerical root finders
+DIST_FROM_BOUND = 10 ** -6
 
 def get_lambda_1(S, N, approx='no'):
     """Solve for lambda_1 from Harte et al. 2008
@@ -24,27 +30,41 @@ def get_lambda_1(S, N, approx='no'):
     assert S > 1, "S must be greater than 1"
     assert N > 0, "N must be greater than 0"
     assert S/N < 1, "N must be greater than S"
-    assert approx == 'yes' or approx == 'no', """approx must be 'yes', 'no', or
+    assert approx == 'yes' or approx == 'no', """approx Bst be 'yes', 'no', or
     blank"""
+    
+    BOUNDS = [0, 1]
+    
+    # Solve for lambda_1 using the substitution x = e**-lambda_1
     if approx == 'no':
-        y = lambda x: 1 / log(1 / (1 - x)) * x / (1 - x) - N / S # x = e**-lambda_1 in B.4
+        y = lambda x: 1 / log(1 / (1 - x)) * x / (1 - x) - N / S
     else:
         y = lambda x: x * log(1 / x) - S / N
-    XSTART = 0.999999
-    exp_neg_lambda_1 = fsolve(y, XSTART)
+    exp_neg_lambda_1 = bisect(y, BOUNDS[0] + DIST_FROM_BOUND, BOUNDS[1] -
+                              DIST_FROM_BOUND)
     lambda_1 = -1 * log(exp_neg_lambda_1)
     return lambda_1
 
-def get_lambda_P(A, A_0, n):
+def get_lambda_P(A, A_0, n_0):
     """Solve for lambda_P from Harte et al. 2008
     
     Keyword arguments:
     A -- the spatial scale of interest
     A_0 -- the maximum spatial scale under consideration
-    n -- the number of individuals of the focal species at scale A_0
+    n_0 -- the number of individuals of the focal species at scale A_0
     
     """
-    assert type(n) is int, "n must be an integer"
+    assert type(n_0) is int, "n must be an integer"
     assert A > 0 and A_0 > 0, "A and A_0 must be greater than 0"
     assert A <= A_0, "A must be less than or equal to A_0"
+
+    BOUNDS = [0, 1]
     
+    # Solve for lambda_P using the substitution x = e**-lambda_P
+    y = lambda x: 1 / (1 - x ** (n_0 + 1)) * (x / (1 - x) - x ** (n_0 +1) *
+                                              (n_0 + 1 / (1 - x))) - (n_0 * A /
+                                                                      A_0)
+    exp_neg_lambda_P = bisect(y, BOUNDS[0] + DIST_FROM_BOUND, BOUNDS[1] -
+                              DIST_FROM_BOUND)
+    lambda_P = -1 * log(exp_neg_lambda_P)
+    return lambda_P
