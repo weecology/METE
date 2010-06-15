814de6d108929e92f150c482c082ea7ae9c85b96
Ethan White <ethan@weecology.org>
2010-06-15 17:00:59 +0000

    Added check for A == A_0/2 in get_lambda_P that returns lambda_P = 0 following Harte et al. 2009.

--- harte_maxent.py
+++ harte_maxent.py
@@ -11,7 +11,7 @@ from scipy.optimize import bisect
 
 # Set the distance from the undefined boundaries of the Lagrangian multipliers
 # to set the upper and lower boundaries for the numerical root finders
-DIST_FROM_BOUND = 10 ** -6
+DIST_FROM_BOUND = 10 ** -10
 
 def get_lambda_1(S, N, approx='no'):
     """Solve for lambda_1 from Harte et al. 2008
@@ -57,14 +57,17 @@ def get_lambda_P(A, A_0, n_0):
     assert type(n_0) is int, "n must be an integer"
     assert A > 0 and A_0 > 0, "A and A_0 must be greater than 0"
     assert A <= A_0, "A must be less than or equal to A_0"
-
-    BOUNDS = [0, 1]
     
-    # Solve for lambda_P using the substitution x = e**-lambda_P
-    y = lambda x: 1 / (1 - x ** (n_0 + 1)) * (x / (1 - x) - x ** (n_0 +1) *
-                                              (n_0 + 1 / (1 - x))) - (n_0 * A /
-                                                                      A_0)
-    exp_neg_lambda_P = bisect(y, BOUNDS[0] + DIST_FROM_BOUND, BOUNDS[1] -
-                              DIST_FROM_BOUND)
-    lambda_P = -1 * log(exp_neg_lambda_P)
+    if A == A_0 / 2:
+        # Special case where A = A_0/2 from Harte et al. 2009 after eq. 6
+        lambda_P = 0
+    else:
+        # Solve for lambda_P using the substitution x = e**-lambda_P
+        BOUNDS = [0, 1]
+        y = lambda x: 1 / (1 - x ** (n_0 + 1)) * (x / (1 - x) - x ** (n_0 +1) *
+                                                  (n_0 + 1 / (1 - x))) - (n_0 * A /
+                                                                          A_0)
+        exp_neg_lambda_P = bisect(y, BOUNDS[0] + DIST_FROM_BOUND, BOUNDS[1] -
+                                  DIST_FROM_BOUND)
+        lambda_P = -1 * log(exp_neg_lambda_P)
     return lambda_P
