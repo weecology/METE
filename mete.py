1ff799cf4890c4cfd21086ba014211373f7b2690
Xiao Xiao <xiao@weecology.org>
2010-12-15 21:13:44 +0000

    Added group of functions to compute expected S in noncontiguous cells with simulation.

--- mete.py
+++ mete.py
@@ -1,9 +1,10 @@
 """Module for fitting and testing Harte et al.'s maximum entropy models"""
 
 from __future__ import division
-from math import log, exp, isnan
+from math import log, exp, isnan, floor, ceil
 from scipy.optimize import bisect, fsolve
 from scipy.stats import logser
+from numpy.random import random_integers
 from numpy import array, e, empty
 import matplotlib.pyplot as plt
 
@@ -241,3 +242,78 @@ def plot_universal_curve(slopes_data):
     plt.hold(True)
     plt.semilogx(NS, z_obs, 'ro')
     plt.show()
+    
+def sim_spatial_one_step(abd_list):
+    """Simulates the abundances of species after bisecting one cell. 
+    
+    Input: species abundances in the original cell. 
+    Output: a list with two sublists containing species abundances in the two halved cells. 
+    Assuming indistinguishable individuals (see Harte et al. 2008). 
+    
+    """
+    abd_half_1 = []
+    abd_half_2 = []
+    for spp in abd_list:
+        if spp == 0:
+            abd_half_1.append(0)
+            abd_half_2.append(0)
+        else:
+            abd_1 = random_integers(0, spp)
+            abd_half_1.append(abd_1)
+            abd_half_2.append(spp - abd_1)
+    abd_halves = [abd_half_1, abd_half_2]
+    return abd_halves
+
+def sim_spatial_whole(S, N, bisec):
+    """Simulates species abundances in all cells given S & N at whole plot level and bisection number. 
+    
+    Output: a list of lists, each sublist contains species abundance in one cell, and x-y coordinate of the cell.
+    Note: bisection number 1 corresponds to no bisection (whole plot). 
+    
+    """
+    abd_pred = get_mete_sad(S, N)[0]
+    abd_prev = [[1, 1, array(abd_pred)]]
+    bisec_num = 1
+    while bisec_num < bisec: 
+        abd_new = []
+        for cell in abd_prev: 
+            x_prev = cell[0]
+            y_prev = cell[1]
+            abd_new_cell = sim_spatial_one_step(cell[2])
+            if bisec_num % 2 != 0: 
+                cell_new_1 = [x_prev * 2 - 1, y_prev, abd_new_cell[0]]
+                cell_new_2 = [x_prev * 2, y_prev, abd_new_cell[1]]
+            else:
+                cell_new_1 = [x_prev, y_prev * 2 - 1, abd_new_cell[0]]
+                cell_new_2 = [x_prev, y_prev * 2, abd_new_cell[1]]
+            abd_new.append(cell_new_1)
+            abd_new.append(cell_new_2)
+        abd_prev = abd_new
+        bisec_num += 1
+    return abd_prev
+
+def sim_spatial_whole_iter(S, N, bisec, coords, n_iter = 10000):
+    """Simulates the bisection n_iter times and gets the aggregated species richness in plots with given coordinates."""
+    max_x = 2 ** ceil((bisec - 1) / 2) 
+    max_y = 2 ** floor((bisec - 1) / 2)
+    if max(array(coords)[:,0]) > max_x or max(array(coords)[:,1]) > max_y:
+        print "Error: Coordinates out of bounds."
+        return float('nan')
+    else: 
+        i = 1
+        S_list = []
+        while i <= n_iter:
+            abd_list = []
+            abd_plot = sim_spatial_whole(S, N, bisec)
+            for i_coords in coords:
+                for j_cell in abd_plot:
+                    if j_cell[0] == i_coords[0] and j_cell[1] == i_coords[1]:
+                        abd_list.append(j_cell[2])
+                        break
+            abd_agg = array(abd_list).sum(axis = 0)
+            S_i = sum(abd_agg != 0)
+            S_list.append(S_i)
+            i += 1
+        S_avg = sum(S_list) / len(S_list)
+        return S_avg
+            
