a82d2e581b71055866912779994cc997f1972c09
Dan McGlinn <danmcglinn@gmail.com>
2012-02-16 15:49:37 -0700

    Added commands to convert the mete_distributions.py into an installed python module

--- setup.py
+++ setup.py
@@ -3,3 +3,7 @@ from distutils.core import setup
 setup(name='mete',
       version='1.x-dev',
       py_modules=['mete'])
+
+setup(name='mete_distributions',
+      version='1.x-dev',
+      py_modules=['mete_distributions'])
