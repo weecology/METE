994ecb6e6c5cad2d98ea703e8d1d5852dfb620f9
dmcglinn <danmcglinn@gmail.com>
2012-11-29 14:03:54 -0700

    Added two functions calc_S_from_Pi and sar_noniterative that are useful for computing the METE expected number of species in the context of the non-iterative species-area relationship.

--- mete.py
+++ mete.py
@@ -254,7 +254,7 @@ def get_mete_sad(S0, N0, beta=None, bin_edges=None):
         pmf = array(binned_pmf)
     predicted_sad = S0 * pmf
     return predicted_sad
-            
+
 def get_lambda_spatialdistrib(A, A0, n0):
     """Solve for lambda_Pi from Harte 2011 equ. 7.50 and 7.51
     
@@ -267,7 +267,6 @@ def get_lambda_spatialdistrib(A, A0, n0):
     assert type(n0) is int, "n must be an integer"
     assert A > 0 and A0 > 0, "A and A0 must be greater than 0"
     assert A <= A0, "A must be less than or equal to A0"
-    
     y = lambda x: x / (1 - x) - (n0 + 1) * x ** (n0 + 1) / (1 - x ** (n0 + 1)) - n0 * A / A0
     if A < A0 / 2:
         # Set the distance from the undefined boundaries of the Lagrangian multipliers
@@ -275,7 +274,7 @@ def get_lambda_spatialdistrib(A, A0, n0):
         BOUNDS = [0, 1]
         DIST_FROM_BOUND = 10 ** -15
         exp_neg_lambda = bisect(y, BOUNDS[0] + DIST_FROM_BOUND,
-                                    BOUNDS[1] - DIST_FROM_BOUND)
+                                   BOUNDS[1] - DIST_FROM_BOUND)
     elif A == A0 / 2:
         #Special case from Harte (2011). See text between Eq. 7.50 and 7.51
         exp_neg_lambda = 1
@@ -284,11 +283,11 @@ def get_lambda_spatialdistrib(A, A0, n0):
         # thus use solution of a logistic equation as the starting point
         exp_neg_lambda = (fsolve(y, - log(A0 / A - 1)))[0] 
     lambda_spatialdistrib = -1 * log(exp_neg_lambda)
-    return lambda_spatialdistrib
+    return lambda_spatialdistrib 
 
-def get_mete_Pi(n, A, n0, A0, lambda_spatialdistrib=None):
+def get_mete_Pi(n, A, n0, A0):
     """
-    Solve for the METE PI distribution from Harte 2011 equ 7.48 and 7.49.
+    Solve for the METE Pi distribution from Harte 2011 equ 7.48 and 7.49.
         
     Arguments:
     n = number of individuals of the focal species at the scale A;
@@ -300,13 +299,61 @@ def get_mete_Pi(n, A, n0, A0, lambda_spatialdistrib=None):
     The probability of observing n out of n0 individuals in a randomly selected quadrat
     of area A out of A0
     """
-    if lambda_spatialdistrib == None:
-        x = exp(-get_lambda_spatialdistrib(A, A0, n0))
-    else:
-        x = exp(-lambda_spatialdistrib)
+    x = exp(-get_lambda_spatialdistrib(A, A0, n0))
     Z_Pi = sum([x ** i for i in range(0, n0 + 1)])    
     mete_Pi = x ** n / Z_Pi
     return mete_Pi
+
+def calc_S_from_Pi(A, A0, S0, N0):
+    """ 
+    Solves for the expected number of species using the non-interative approach
+    Harte 2011 equ C.1
+    
+    Arguments:
+    A = the spatial scale of interest;
+    A0 = the maximum spatial scale under consideration;
+    S0 = the total number of species that occurs in A0
+    N0 = the total number of individuals that occurs in A0
+
+    Returns:
+    The expected number of species inferred from the anchor scale
+    """
+    beta = get_beta(S0, N0)
+    def calc_prob_joint(beta, n, A, A0):
+        prob_occur = 1 - get_mete_Pi(0, A, n, A0)
+        prob_n_indiv = exp(-beta * n) / (n * log(beta ** -1))
+        prob_joint = prob_n_indiv * prob_occur
+        return prob_joint
+    prob_of_occurrence = [calc_prob_joint(beta, n, A, A0) for n in range(1, N0 + 1)]
+    S = S0 * sum(prob_of_occurrence)
+    return S
+
+def sar_noniterative(Avals, A0, S0, N0):
+    """ Computes the downscaled METE noninterative species-area relationship (SAR)
+    Harte 2011 equ C.1\
+    
+    Arguments:
+    Avals: the spatial scales of interest, must be greater than zero and less than A0;
+    A0 = the maximum spatial scale under consideration;
+    S0 = the total number of species in A0;
+    N0 = the total number of individuals in A0;
+    
+    Returns:
+    A numpy array the first row contains the Avals, and the second row contains the expected
+    S values
+    """
+    assert S0 <= 5, "S0 must be greater than 5"
+    A_ok = [i > 0 and i < A0 for i in Avals]
+    if False in A_ok:
+        print "Warning: will only compute S for Areas that are greater than zero and less than A0"
+        Avals  = [Avals[i] for i in which(A_ok)]
+    Svals = [calc_S_from_Pi(A, A0, S0, N0) for A in Avals]
+    Svals.append(S0)
+    Avals.append(A0)
+    out = list()
+    out.append(Avals)
+    out.append(Svals)
+    return out
 
 def get_mete_rad(S, N, beta=None, beta_dict={}):
     """Use beta to generate SAD predicted by the METE
@@ -440,7 +487,7 @@ def predicted_slope(S, N):
     else:
         print("Error in downscaling. Cannot find root.")
         return float('nan')
-    
+
 def get_slopes(site_data):
     """get slopes from various scales, output list of area slope and N/S
     
@@ -471,7 +518,7 @@ def get_slopes(site_data):
         else:
             break
     return Zvalues    
-    
+
 def plot_universal_curve(slopes_data):
     """plots ln(N/S) x slope for empirical data and MaxEnt predictions. 
     
@@ -516,7 +563,7 @@ def heap_pmf(A, n0, A0):
     """
     pmf = [heap_prob(n, A, n0, A0) for n in range(0, n0 + 1)]
     return pmf
-    
+
 def binomial(n, k):
     return factorial(n) / (factorial(k) * factorial(n - k))
 
@@ -535,7 +582,7 @@ def get_heap_dict(n, A, n0, A0, plist=[0, {}]):
     Determines the HEAP probability for n given A, n0, and A0
     Uses equation 4.15 in Harte 2011
     Returns a list with the first element is the probability of n individuals 
-    beting observed in a quadrat of area A, and the second element is a 
+    being observed in a quadrat of area A, and the second element is a 
     dictionary that was built to compute that probability
     """
     i = int(log(A0 / A,2))
@@ -671,7 +718,7 @@ def sim_spatial_whole_iter(S, N, bisec, coords, n_iter = 10000):
             i += 1
         S_avg = sum(S_list) / len(S_list)
         return S_avg
-    
+
 def community_energy_pdf(epsilon, S0, N0, E0):
     lambda1 = get_lambda1()
     lambda2 = get_lambda2()
