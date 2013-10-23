f8b2b72bf7cf799769f06db16b49df66f6c5e4ba
Ethan White <ethan@weecology.org>
2013-10-22 21:13:43 -0400

    Update setup.py to use setuptools and include more information

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='mete',
-      version='1.x-dev',
-      py_modules=['mete'])
-
-setup(name='mete_distributions',
-      version='1.x-dev',
-      py_modules=['mete_distributions'])
+setup(name = 'mete',
+      version= '0.1',
+      description = 'Tools for analying the Maximum Entropy Theory of Ecology',
+      author = "Ethan White, Dan McGlinn, Xiao Xiao, Sarah Supp, and Katherine Thibault",
+      url = 'https://github.com/weecology/mete',
+      packages = find_packages(),
+      package_data = {'mete': ['beta_lookup_table.pck']},
+      license = 'MIT',
+)
