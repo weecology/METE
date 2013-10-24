64d4c0470bd924c15f04a94b794ad20a29a7c5ef
Ethan White <ethan@weecology.org>
2013-10-23 21:36:19 -0400

    Allow setup.py to work with both setuptools and distutils

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
-from setuptools import setup, find_packages
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(name = 'mete',
       version= '0.1',
       description = 'Tools for analying the Maximum Entropy Theory of Ecology',
       author = "Ethan White, Dan McGlinn, Xiao Xiao, Sarah Supp, and Katherine Thibault",
       url = 'https://github.com/weecology/mete',
-      packages = find_packages(),
+      packages = ['mete', 'mete_distributions'],
       package_data = {'mete': ['beta_lookup_table.pck']},
       license = 'MIT',
 )
