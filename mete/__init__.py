9c13fedd455ce156611aeadae83ec0c0d3835c6d
Ethan White <ethan@weecology.org>
2013-10-23 21:35:43 -0400

    Updating __init__ files to properly conduct imports

---
+++ mete/__init__.py
@@ -0,0 +1,10 @@
+"""
+mete: Tools for analyzing the Maximum Entropy Theory of Ecology
+
+Reference: John Harte. (2011). Maximum entropy and ecology: a theory
+of abundance, distribution, and energetics. Oxford University Press.
+
+"""
+
+
+from .mete import *
---
+++ mete_distributions/__init__.py
@@ -0,0 +1,9 @@
+"""
+mete_distributions: distributions from the Maximum Entropy Theory of Ecology
+
+Reference: John Harte. (2011). Maximum entropy and ecology: a theory
+of abundance, distribution, and energetics. Oxford University Press.
+
+"""
+
+from .mete_distributions import *
